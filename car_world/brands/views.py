b30e63b5ab583f94ceb6b98b8618532f79f533cf
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-12-23 22:19:36 +0600

    first commit

---
+++ car_world/brands/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, redirect
+from . import forms
+
+# Create your views here.
---
+++ car_world/cars/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from brands.models import Brands
+# Create your models here.
+
+
+class Car(models.Model):
+    brand = models.ForeignKey(Brands, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='cars/uploads')
+    price = models.FloatField()
+    quantity = models.PositiveIntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    buyers = models.ManyToManyField(
+        User, related_name='bought_cars', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    car = models.ForeignKey(
+        Car, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+
